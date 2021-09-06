037e43950a5038d0d98ad37d399f1431b876a1b0
Abhishek Rathore <rathore.abhishek87@gmail.com>
2021-09-06 16:06:40 +0530

    added initial routing using Link/Switch/Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/">Home </Link>
+        </li>
+        <li>
+          <Link to="/post">Post </Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/post" component={Post}></Route>
+        <Route render={() => <h1>Page Not Found</h1>}></Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Post() {
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
     </div>
   );
 }
